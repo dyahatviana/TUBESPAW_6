7cd10fcba34a2c22902649b7a51032241fdbb785
dyahatviana <ayaacvia4@gmail.com>
2024-12-26 16:21:53 +0700

    first commit

---
+++ resources/views/frontend/inc/breadcrumb.blade.php
@@ -0,0 +1,19 @@
+<div class="bg-light py-3">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 mb-0">
+                <a href="{{ route('index') }}">Home</a>
+                @if (!empty($breadcrumb['pages']))
+                    @foreach ($breadcrumb['pages'] as $br)
+                        <span class="mx-2 mb-0">/</span>
+                        <a href="{{ $br['link'] }}">{{ $br['name'] }}</a>
+                    @endforeach
+                @endif
+                @if (!empty($breadcrumb['active']))
+                    <span class="mx-2 mb-0">/</span>
+                    <strong class="text-black">{{ $breadcrumb['active'] }}</strong>
+                @endif
+            </div>
+        </div>
+    </div>
+</div>
